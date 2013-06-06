1a6bb2cc01038c075a091e0b76616fe16c9df621
Garrick van Buren <email@garrickvanburen.com>
2013-06-06 14:07:53 -0500

    getting left nav to display as expected, adding js to force add 'pure-form' classes to forms and buttons

--- assets/scripts/filament.js
+++ assets/scripts/filament.js
@@ -1,3 +1,5 @@
 jQuery(document).ready(function() {
-
+  jQuery('form').addClass('pure-form');
+  jQuery('input[type="submit"]').addClass('pure-button');
+  jQuery('input[type="submit"]').addClass('notice');
 });
--- filament.skin.php
+++ filament.skin.php
@@ -39,38 +39,34 @@ class FilamentTemplate extends BaseTemplate {
 		<div class="pure-menu pure-menu-open">
 			<a href="<?php echo $this->data['nav_urls']['mainpage']['href']; ?>" class="pure-menu-heading"><?php echo $this->text('sitename'); ?></a>
 			<ul>
-			      <li class="menu-item-divided">
-		        	<form action="<?php $this->text( 'wgScript' ); ?>" id="searchform" class="mw-search">
-		        		<?php echo $this->makeSearchInput(array('placeholder' => 'Search...', 'id' => 'searchInput') ); ?>
+			      <li>
+		        	<form action="<?php $this->text( 'wgScript' ); ?>" id="searchform" class="mw-search pure-form">
+		        		<?php echo $this->makeSearchInput(array('placeholder' => 'Search...', 'id' => 'searchInput', 'class' => 'pure-input-rounded') ); ?>
 		        	</form>
 		        </li>
-	
+			</ul>
+			<a href="#" class="menu-item-divided"><?php echo htmlspecialchars( $box['header'] ); ?></a>
+			<ul>
 				<?php foreach ( $this->getSidebar() as $boxName => $box ) { ?>
-					<li class="menu-item-divided"  id='<?php echo Sanitizer::escapeId( $box['id'] ) ?>'<?php echo Linker::tooltip( $box['id'] ) ?>>
-						<a href="#"><?php echo htmlspecialchars( $box['header'] ); ?></a>
 						<?php if ( is_array( $box['content'] ) ) { ?>
-							<ul class="dropdown">
-								<?php foreach ( $box['content'] as $key => $item ) { echo $this->makeListItem( $key, $item ); } ?>
-	       					</ul>
+							<?php foreach ( $box['content'] as $key => $item ) { echo $this->makeListItem( $key, $item ); } ?>
 						<?php }  ?>
-					</li>
 				<?php } ?>
+			</ul>
 
 				<?php if ($wgUser->isLoggedIn()): ?>
-				<li class="menu-item-divided"><a href="#"><?php $this->msg('views') ?></a>
+				<a href="#" class="menu-item-divided"><?php $this->msg('views') ?></a>
 					<ul class="dropdown">
 					<?php foreach( $this->data['content_actions'] as $key => $item ) { echo $this->makeListItem($key, $item); } ?>
 					</ul>
-				</li>
-				<li class="menu-item-divided"><a href="#">Personal</a>
+
+				<a href="#" class="menu-item-divided">Personal</a>
 					<ul class="dropdown">
 					<?php foreach ( $this->getPersonalTools() as $key => $item ) { echo $this->makeListItem($key, $item); } ?>
 					</ul>
-				</li>
+
 				<?php endif; ?>
-				<li class="menu-item-divided">
 					<?php if ($wgUser->isLoggedIn()): ?>
-						<a href=""><?php echo Linker::link(Title::newFromText('Special:UserLogout'), 'Sign Out'); ?></a>
 					<?php else: ?>
 						<?php if (isset($this->data['personal_urls']['anonlogin'])): ?>
 							<a href="<?php echo $this->data['personal_urls']['anonlogin']['href']; ?>">Sign In</a>
@@ -80,8 +76,7 @@ class FilamentTemplate extends BaseTemplate {
 							<?php echo Linker::link(Title::newFromText('Special:UserLogin'), 'Sign In'); ?>
 						<?php endif; ?>
 					<?php endif; ?>
-				</li>
-	  		</ul>
+
 		</div>
 	</div>
 
@@ -89,8 +84,8 @@ class FilamentTemplate extends BaseTemplate {
 		<?php if ( $this->data['sitenotice'] ) { ?><div id="siteNotice" class="row notice large-12 columns"><?php $this->html( 'sitenotice' ); ?></div><?php } ?>
 		<?php if ( $this->data['newtalk'] ) { ?><div class="usermessage row notice large-12 columns"><?php $this->html( 'newtalk' ); ?></div><?php } ?>
 		<div id="mw-js-message" style="display:none;"></div>
-		<div class="pure-u-1">
-			<h1 class="pure-u"><?php $this->html('title') ?></h1>
+		<div class="header pure-u-1">
+			<h1 class="pure-u-1"><?php $this->html('title') ?></h1>
 			<h2 class="pure-u"><?php $this->html('subtitle') ?></h2>
 		</div>
 		<div class="content">
@@ -99,11 +94,15 @@ class FilamentTemplate extends BaseTemplate {
 	    	<?php $this->html('dataAfterContent'); ?>
 	 	</div>
 		<div class="legal pure-g-r">
-			<div class="legal">
+			<div class="pure-u-2-5">
+				<ul>
 				<?php foreach ( $this->getFooterLinks( "flat" ) as $key ) { ?>
 				        <li><?php $this->html( $key ) ?></li>
 				<?php } ?>
 				</ul>
+			</div>
+
+			<div class="pure-u-2-5">
 				<ul> <?php foreach ( $this->getFooterIcons( "nocopyright" ) as $blockName => $footerIcons ) { ?>
 		        <li><?php foreach ( $footerIcons as $icon ) { ?>
 		            <?php echo $this->getSkin()->makeFooterIcon( $icon, 'withoutImage' ); ?>
@@ -112,6 +111,7 @@ class FilamentTemplate extends BaseTemplate {
 						<?php } ?>
 				</ul>
 			</div>
+
 		</div>
 
 		<div id="mw-js-message" style="display:none;"></div>
